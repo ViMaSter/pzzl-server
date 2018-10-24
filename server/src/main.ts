59179d3044505c4ac30162f977e19c07f5e820a0
Vincent Mahnke <vincent@mahn.ke>
2018-10-24 19:56:17 +0200

    Fixed usage of promise

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,5 +3,4 @@ import {SessionServer} from "SessionServer/SessionServer"
 import {PageServer} from "PageServer/PageServer"
 
 const sessionServer : SessionServer<SessionData> = new SessionServer<SessionData>(parseInt(process.env.PORT || "") || 7996);
-const pageServer : PageServer = new PageServer(parseInt(process.env.PORT || "") || 7995, "html");
-
+const pageServer : Promise<PageServer> = PageServer.Create(parseInt(process.env.PORT || "") || 7995, "html");
